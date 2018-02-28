3200aff85712ca75e665f04b7f0a7660f40f5817
kig3434 <kig3434@aol.com>
2018-02-27 18:35:56 -0700

    Homework 6

--- R/hello.R
+++ R/hello.R
@@ -13,12 +13,144 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-  x <- rnorm(100000)
-  hist(x)
-  mean(x)
-print(x)
-sss
+#Hw 6. Function and packages
+
+library(tidyverse)
+library(ggthemes)
+
+#3. Create and document a function that generates a density plot for a specific variable in a data.frame
+
+
+df <- data.frame(
+  a = rnorm(1000, 500, 10),
+  b = rnorm(200),
+  c = rnorm(20),
+  d = rnorm(1000000)
+)
+
+d <- function(y) {
+  den <- hist(y)
+  avg <- mean(y)
+  sd <- sd(y)
+  bplot <- boxplot(y, horizontal = TRUE)
+  rng <- range(y)
+  print(avg)
+  print(sd)
+  print(rng)
 }
+
+d(df$a)
+
+
+#4. Create and document a new customer ggplot theme function (Hint: Look at an existing theme and modify it)
+
+
+mpg
+
+theme_tt <- function () {
+  theme_dark(base_size=10, base_family="sans") %+replace%
+    theme(
+      panel.background  = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_line(colour = "black", size=0.5),
+      axis.text.x = element_text(angle=50, size=10, vjust=0.5),
+      axis.text.y = element_text(angle=50, size=10, vjust=0.5),
+      axis.title.x = element_text(color="forestgreen", vjust=-0.35),
+      axis.title.y = element_text(color="darksalmon" , vjust=0.35)
+    )
+}
+
+ggplot(mpg, aes(x = hwy, y = cty, color = class)) +
+  geom_point() +
+  geom_smooth(method= 'loess', span = 1, se = FALSE, color = 'red') +
+  xlab("Highway Miles") +
+  ylab("City Miles") +
+  theme_tt()
+
+
+
+
+
+#4. Create a function that provides the top n number of a grouped variable by counts (Hint: use dplyr and for the group by use this line of code so you can pass in a character vector "groupings" for the group by)
+#group_by_at(vars(one_of(groupings)))
+
+
+top10(mpg, model)
+
+top10 <- function(df, var) {
+  df %>%
+    group_by_(var) %>%
+    summarise(count = n()) %>%
+    arrange(desc(count)) %>%
+    head(10)
+
+}
+top10(mpg, "manufacturer")
+
+
+
+
+
+
+
+#5. Create 4 other general functions (you can use the ones from lecture and/or #create your own)
+#Function ideas
+#Power analysis
+#Bootstrap
+#Other functions that use dplyr code or ggplot2
+
+##1
+library(e1071)
+descriptives <- function(p) {
+  m <- mean(p)
+  s <- sd(p)
+  rng <- range(p)
+  med <- median(p)
+  skew <-skewness(p)
+  Min <- min(p)
+  Max <- max(p)
+  tibble(c(m,s, rng, med, skew, Min, Max))
+
+}
+descriptives(df$a)
+
+##2
+tt_cor <- function(x, y){
+  tt <- t.test(x,y)
+  c <-cor(x,y)
+  print(c(tt, c))
+}
+tt_cor(mpg$year, mpg$hwy)
+
+
+##3
+USdollar_to_euro <- function(m) {
+  Euro <- m*.81
+  print(Euro)
+  print("exchange rate as of 2/26/2018")
+}
+
+USdollar_to_euro(5)
+
+
+
+##4
+craps <- function(x, y) {
+  d1 <- sample(1:6, 1)
+  d2 <-sample(1:6, 1)
+  print(d1)
+  print(d2)
+  Sum <- sum(d1, d2)
+  print(Sum)
+  if(Sum =='7'){
+    print('CRAPS')
+  }
+  else if(Sum =='11'){
+    print('CRAPS')
+  }
+}
+
+craps(roll)
+
+#6. Commit and push your code to Github
 
